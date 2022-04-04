6359f27bc7ca32f918748ddaf73f8d399e8d91d7
Alan Jian <alanjian85@outlook.com>
2022-04-04 22:05:17 +0800

    Implied opcodes

--- nes/src/cpu.cc
+++ nes/src/cpu.cc
@@ -9,12 +9,66 @@ CPU::CPU(Bus& bus)
 
 void CPU::step() {
     auto opcode = fetch();
+    if (execImplied(opcode))
+        return;
     if (execGroup0(opcode))
         return;
     if (execGroup1(opcode))
         return;
     if (execGroup2(opcode))
         return;
+}
+
+bool CPU::execImplied(std::uint8_t opcode) {
+    switch (opcode) {
+        case 0x08: // PHP
+            break;
+        case 0x28: // PLP
+            break;
+        case 0x48: // PHA
+            break;
+        case 0x68: // PLA
+            break;
+        case 0x88: // DEY
+            break;
+        case 0xA8: // TAY
+            break;
+        case 0xC8: // INY
+            break;
+        case 0xE8: // INX
+            break;
+        case 0x18: // CLC
+            break;
+        case 0x38: // SEC
+            break;
+        case 0x58: // CLI
+            break;
+        case 0x78: // SEI
+            break;
+        case 0x98: // TYA
+            break;
+        case 0xB8: // CLV
+            break;
+        case 0xD8: // CLD
+            break;
+        case 0xF8: // SED
+            break;
+        case 0x8A: // TXA
+            break;
+        case 0x9A: // TXS
+            break;
+        case 0xAA: // TAX
+            break;
+        case 0xBA: // TSX
+            break;
+        case 0xCA: // DEX
+            break;
+        case 0xEA: // NOP
+            break;
+        default:
+            return false;
+    }
+    return true;
 }
 
 bool CPU::execGroup0(std::uint8_t opcode) {
