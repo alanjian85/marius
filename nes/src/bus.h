549926f00f18e84da6319886dee832c781e4a09f
Alan Jian <alanjian85@outlook.com>
2022-04-04 12:15:10 +0800

    Provide bus interface

---
+++ nes/src/bus.h
@@ -0,0 +1,21 @@
+#ifndef NES_BUS_H_
+#define NES_BUS_H_
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+namespace nes {
+    class Bus {
+    public:
+        Bus();
+
+        std::byte read(std::uint16_t addr) const;
+
+        void write(std::uint16_t addr, std::byte val);
+    private:
+        std::array<std::byte, 0x800> ram_;
+    };
+}
+
+#endif // NES_BUS_H_
