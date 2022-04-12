e6e5b69aca573c8206fa21fcc8395c79f6ead228
alanjian85 <alanjian85@outlook.com>
2022-04-12 23:40:37 +0800

    Nametable mirroring

--- nes/src/components/ppu_bus.cc
+++ nes/src/components/ppu_bus.cc
@@ -11,7 +11,21 @@ std::uint8_t PpuBus::read(std::uint16_t addr) const {
     if (addr < 0x2000) {
         return mapper_.readChr(addr);
     } else if (addr < 0x3F00) {
-        return ram_[addr & 0xFFF];
+        std::uint16_t nametable = addr & 0xFC00;
+        switch (mapper_.getMirroring()) {
+            case Mirroring::kHorizontal:
+                if (nametable == 0x2000 || nametable == 0x2400) {
+                    return ram_[addr & 0x3FF];
+                } else {
+                    return ram_[addr & 0x3FF + 0x400];
+                }
+            case Mirroring::kVertical:
+                if (nametable == 0x2000 || nametable == 0x2800) {
+                    return ram_[addr & 0x3FF];
+                } else {
+                    return ram_[addr & 0x3FF + 0x400];
+                }
+        }
     } else {
         return ram_[addr & 0x001F];
     }
@@ -22,7 +36,21 @@ void PpuBus::write(std::uint16_t addr, std::uint8_t val) {
     if (addr < 0x2000) {
         mapper_.writeChr(addr, val);
     } else if (addr < 0x3F00) {
-        ram_[addr & 0x0FFF] = val; 
+        std::uint16_t nametable = addr & 0xFC00;
+        switch (mapper_.getMirroring()) {
+            case Mirroring::kHorizontal:
+                if (nametable == 0x2000 || nametable == 0x2400) {
+                    ram_[addr & 0x3FF] = val;
+                } else {
+                    ram_[addr & 0x3FF + 0x400] = val;
+                }
+            case Mirroring::kVertical:
+                if (nametable == 0x2000 || nametable == 0x2800) {
+                    ram_[addr & 0x3FF] = val;
+                } else {
+                    ram_[addr & 0x3FF + 0x400] = val;
+                }
+        }
     } else {
         palette_[addr & 0x001F] = val;
         std::uint8_t nibble = addr & 0xF;
