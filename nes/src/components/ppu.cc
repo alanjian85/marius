198e177934dcd7f6ab6fae4f2776f1870e3e82fb
alanjian85 <alanjian85@outlook.com>
2022-04-14 21:35:29 +0800

    Attribute table palette indexing

--- nes/src/components/ppu.cc
+++ nes/src/components/ppu.cc
@@ -37,12 +37,28 @@ void Ppu::reset() {
 void Ppu::cycle() {
     if (show_background_ && cycle_ == 0 && scanline_ < 240) {
         for (int x = 0; x < 256; ++x) {
-            std::uint8_t tile = bus_.read(0x2000 + scanline_ / 8 * 32 + x / 8);
+            int tile_x = x / 8, tile_y = scanline_ / 8;
+            int block_x = x / 32, block_y = scanline_ / 32;
+
+            std::uint8_t tile = bus_.read(0x2000 + tile_y * 32 + tile_x);
+            std::uint8_t block = bus_.read(0x23C0 + block_y * 8 + block_x);
+            std::uint8_t palette = 0x00;
+            if (tile_x % 4 < 2 && tile_y % 4 < 2) {
+                palette = block >> 0 & 0x03;
+            } else if (tile_x % 4 >= 2 && tile_y % 4 < 2) {
+                palette = block >> 2 & 0x03;
+            } else if (tile_x % 4 < 2 && tile_y % 4 >= 2) {
+                palette = block >> 4 & 0x03;
+            } else {
+                palette = block >> 6 & 0x03;
+            }
+
             bool bit0 = bus_.read(0x1000 + tile * 16 + scanline_ % 8) & 0x80 >> x % 8;
             bool bit1 = bus_.read(0x1000 + tile * 16 + 8 + scanline_ % 8) & 0x80 >> x % 8;
             std::uint8_t index = bit0 | bit1 << 1;
+
             if (index != 0x00) {
-                framebuffer_.setPixel(x, scanline_, kPalette[bus_.read(0x3F00 + index)]);
+                framebuffer_.setPixel(x, scanline_, kPalette[bus_.read(0x3F00 + palette * 4 + index)]);
             } else {
                 framebuffer_.setPixel(x, scanline_, 0x000000FF);
             }
